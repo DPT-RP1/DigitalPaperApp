efd4247436c21b4975bcb25f49192c828f54cb99
pierre <xpierro@gmail.com>
2020-04-05 18:51:37 +0800

    Migrate if already registered on another app

--- src/main/java/net/sony/dpt/persistence/RegistrationTokenStore.java
+++ src/main/java/net/sony/dpt/persistence/RegistrationTokenStore.java
@@ -7,47 +7,143 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
 
 public class RegistrationTokenStore {
 
-    private static final Path privateKeyPath = Path.of("id_rsa");
-    private static final Path clientIdPath = Path.of("client_id.dat");
-    private static final Path certificatePath = Path.of("cert.pem");
-    private static final Path applicationPath = Path.of(".dpt");
-    private final Path storagePath;
+    public enum TokenStoreLocation {
+        DEFAULT(".dpt", "id_rsa", "cert.pem", "client_id.dat"),
+        PYTHON(".dpapp", "privatekey.dat", null, "deviceid.dat"),
+        OFFICIAL_WINDOWS("AppData/Roaming/Sony Corporation/Digital Paper App/DigitalPaperApp", "privatekey.dat", null, "deviceid.dat"),
+        OFFICIAL_MAC("Library/Application Support/Sony Corporation/Digital Paper App/DigitalPaperApp", "privatekey.dat", null, "deviceid.dat");
 
-    public RegistrationTokenStore(Path storagePath) {
-        this.storagePath = storagePath.resolve(applicationPath);
+        private final Path applicationPath;
+        private final Path privateKeyName;
+        private Path certName;
+        private final Path clientIdName;
+
+        TokenStoreLocation(String applicationPath, String privateKeyName, String certName, String clientIdName) {
+            this.applicationPath = Path.of(applicationPath);
+            this.privateKeyName = Path.of(privateKeyName);
+            if (certName != null) this.certName = Path.of(certName);
+            this.clientIdName = Path.of(clientIdName);
+        }
+
+        public static TokenStoreLocation[] nonDefaultLocations() {
+            return Arrays.stream(values()).filter(tokenStoreLocation -> tokenStoreLocation != DEFAULT).toArray(TokenStoreLocation[]::new);
+        }
+    }
+
+    private final Path root;
+
+    public RegistrationTokenStore(Path storageRoot) {
+        this.root = storageRoot;
     }
 
     public void storeRegistrationToken(RegistrationResponse registrationResponse) throws IOException {
-        Files.createDirectories(storagePath);
+        storeRegistrationToken(registrationResponse, TokenStoreLocation.DEFAULT);
+    }
+
+
+    public void storeRegistrationToken(RegistrationResponse registrationResponse, TokenStoreLocation tokenStoreLocation) throws IOException {
+        Path storagePath = root.resolve(tokenStoreLocation.applicationPath);
+        Path privateKeyPath = tokenStoreLocation.privateKeyName;
+        Path clientIdPath = tokenStoreLocation.clientIdName;
+        Path certificatePath = tokenStoreLocation.certName;
+
+        Path lastWorkspace = findLastWorkspacePath(tokenStoreLocation);
+
+        Files.createDirectories(storagePath.resolve(lastWorkspace));
 
         try {
-            Files.createFile(storagePath.resolve(privateKeyPath));
-            Files.createFile(storagePath.resolve(clientIdPath));
-            Files.createFile(storagePath.resolve(certificatePath));
-        } catch (FileAlreadyExistsException ignored) {
-        }
+            Files.createFile(storagePath.resolve(lastWorkspace).resolve(privateKeyPath));
+            Files.createFile(storagePath.resolve(lastWorkspace).resolve(clientIdPath));
+            if (certificatePath != null) Files.createFile(storagePath.resolve(lastWorkspace).resolve(certificatePath));
+        } catch (FileAlreadyExistsException ignored) { }
 
-        Files.write(storagePath.resolve(privateKeyPath), registrationResponse.getPrivateKey().getBytes(StandardCharsets.UTF_8));
-        Files.write(storagePath.resolve(clientIdPath), registrationResponse.getClientId().getBytes(StandardCharsets.UTF_8));
-        Files.write(storagePath.resolve(certificatePath), registrationResponse.getPemCertificate().getBytes(StandardCharsets.UTF_8));
+        Files.write(storagePath.resolve(lastWorkspace).resolve(privateKeyPath), registrationResponse.getPrivateKey().getBytes(StandardCharsets.UTF_8));
+        Files.write(storagePath.resolve(lastWorkspace).resolve(clientIdPath), registrationResponse.getClientId().getBytes(StandardCharsets.UTF_8));
+        if (certificatePath != null) Files.write(storagePath.resolve(lastWorkspace).resolve(certificatePath), registrationResponse.getPemCertificate().getBytes(StandardCharsets.UTF_8));
     }
 
     public RegistrationResponse retrieveRegistrationToken() throws IOException {
-        String privateKey = Files.readString(storagePath.resolve(privateKeyPath));
-        String clientId = Files.readString(storagePath.resolve(clientIdPath));
-        String certificate = Files.readString(storagePath.resolve(certificatePath));
+        return retrieveRegistrationToken(TokenStoreLocation.DEFAULT);
+    }
+
+    public RegistrationResponse retrieveRegistrationToken(TokenStoreLocation tokenStoreLocation) throws IOException {
+        Path storagePath = root.resolve(tokenStoreLocation.applicationPath);
+        Path privateKeyPath = tokenStoreLocation.privateKeyName;
+        Path clientIdPath = tokenStoreLocation.clientIdName;
+        Path certificatePath = tokenStoreLocation.certName;
+
+        Path lastWorkspace = findLastWorkspacePath(tokenStoreLocation);
+
+        String privateKey = Files.readString(storagePath.resolve(lastWorkspace).resolve(privateKeyPath));
+        String clientId = Files.readString(storagePath.resolve(lastWorkspace).resolve(clientIdPath));
+
+        // The certificate is optional, as it's only used for SSL with hostname validation
+        Path certFullPath = storagePath.resolve(lastWorkspace).resolve(certificatePath);
+        String certificate =
+                Files.exists(certFullPath)
+                ? Files.readString(certFullPath)
+                : null;
 
         return new RegistrationResponse(certificate, privateKey, clientId);
     }
 
-    public boolean registered() {
+    public boolean registered(TokenStoreLocation tokenStoreLocation) {
+        Path storagePath = root.resolve(tokenStoreLocation.applicationPath);
+        Path privateKeyPath = tokenStoreLocation.privateKeyName;
+        Path clientIdPath = tokenStoreLocation.clientIdName;
+        Path lastWorkspace;
+        try {
+            lastWorkspace = findLastWorkspacePath(tokenStoreLocation);
+        } catch (IOException ignored) {
+            return false;
+        }
+
         return Files.exists(storagePath)
+                && Files.exists(lastWorkspace)
                 && Files.exists(storagePath.resolve(privateKeyPath))
-                && Files.exists(storagePath.resolve(clientIdPath))
-                && Files.exists(storagePath.resolve(certificatePath));
+                && Files.exists(storagePath.resolve(clientIdPath));
     }
 
+    public Path findLastWorkspacePath(TokenStoreLocation tokenStoreLocation) throws IOException {
+        switch (tokenStoreLocation) {
+            case PYTHON:
+            case DEFAULT:
+                return Path.of("");
+            case OFFICIAL_MAC:
+            case OFFICIAL_WINDOWS:
+                Path applicationPath = root.resolve(tokenStoreLocation.applicationPath);
+                Path lastWorkspaceIdDescriptorPath = Path.of("lastworkspaceid.dat");
+                return Path.of(
+                    Files.readString(
+                        applicationPath.resolve(
+                            lastWorkspaceIdDescriptorPath
+                        )
+                    )
+
+                );
+            default:
+                throw new IllegalArgumentException("Unknown location " + tokenStoreLocation.name());
+        }
+    }
+
+    /**
+     * Looks for registration everywhere possible
+     * @return true if registered anywhere and migrated if not registered on the default path, false otherwise
+     */
+    public boolean registered() throws IOException {
+        if (registered(TokenStoreLocation.DEFAULT)) return true; // We're already registered on this application
+        for (TokenStoreLocation tokenStoreLocation : TokenStoreLocation.nonDefaultLocations()) {
+            if (registered(tokenStoreLocation)) {
+                storeRegistrationToken(retrieveRegistrationToken(tokenStoreLocation));
+                return registered(TokenStoreLocation.DEFAULT); // We re-check that this time we're default-registered
+            }
+        }
+        return false;
+    }
 }
