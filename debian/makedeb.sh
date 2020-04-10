cea2ba2fada3ab43d6d1aecdfa97c01f33b2d963
pierre <xpierro@gmail.com>
2020-04-10 19:30:17 +0800

    More debian fixes

--- debian/makedeb.sh
+++ debian/makedeb.sh
@@ -34,10 +34,20 @@ mkdir "${root_folder}/DEBIAN"
 #TODO: filtering for the version ?
 cp -R DEBIAN "${root_folder}/"
 cp copyright "${root_folder}/usr/share/doc/dpt/"
+
+echo "Building changelog"
+cp changelog changelog.bak
+gbp dch --ignore-branch --since=1c23aca0070f0f7a9d9f064b2d172165eb8060c2 -R --full --spawn-editor=never --git-author
+
+cp changelog.gz "${root_folder}/usr/share/doc/dpt/changelog.Debian.gz"
+cp changelog.gz "${root_folder}/usr/share/doc/dpt/changelog.gz"
+
+echo "Building package"
 dpkg-deb --root-owner-group --build "${root_folder}"
 
+echo "Cleaning up"
 rm -rf "${root_folder}"
-
+cp changelog.bak changelog
 echo "Debian package built, now installing"
 sudo dpkg -r dpt
 sudo dpkg -i dpt_1.0-1.deb
