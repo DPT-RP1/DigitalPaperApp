a4cebf8b9b6a39c69b0d19c15ec154e7a5837ef4
pierre <xpierro@gmail.com>
2020-05-02 11:02:05 +0800

    Add adb patch

--- tools/bootimg/diff.sh
+++ tools/bootimg/diff.sh
@@ -5,6 +5,6 @@
 # $2 folder2
 
 diff -qr "$1/initramfs/" "$2/initramfs/"
-
+diff -ruN "$1/initramfs/" "$2/initramfs/" > initramfs.patch
 # sudo apt install meld for a GUI
 # diff -y for CLI side to side
